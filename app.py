8b64de5aa55ac6a13781f70c7b6dc9cf0e3c49f6
PRATIK17492 <pratikpreetam1714@gmail.com>
2025-10-19 10:51:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit
 import os, json
 from datetime import datetime
